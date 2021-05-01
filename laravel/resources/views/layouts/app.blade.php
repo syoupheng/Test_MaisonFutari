d89bd49c5e6d2ac9777f3c76f231c210e99a1ac9
Pheng <do.syoupheng@gmail.com>
2021-05-01 20:10:04 +0200

    Changement du nom de l'appli dans la vue de base

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>JapAdvisor</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -25,7 +25,7 @@
     <div id="page-container">
         <header>
             <a id="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
+                JapAdvisor
             </a>
             <nav>
                 <ul id="nav_list">
